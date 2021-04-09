d6ccec7086a9464ae27053238386eadffc481eb5
Craig Neff <ifly12610@gmail.com>
2021-04-09 00:54:46 -0600

    practicing some jquerry and workign with DOM

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 "use strict";
 
+let newGameBtn = document.querySelector("#newgame");
+let player1Info = document.querySelector("player--0");
+let player2Info = document.querySelector("player--1");
 let playing = true;
+
+newGameBtn.addEventListener("click", () => {
+    
+});
+function newPlayerInfo()
+
+function chooseFirst() {
+  let flip = Math.trunc(Math.random() * 2 + 1);
+  console.log(flip);
+  if (flip == 1) {
+    console.log("Player 1");
+    return "Player1";
+  } else {
+    console.log("Player 2 ");
+    return "Player2";
+  }
+}
+
+chooseFirst();
